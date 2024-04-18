fe13acfc4bb6c82b9711d63f69c641d9d1cb5377
Riteek Rakesh <riteek7.12@gmail.com>
2024-04-18 22:43:10 +0530

    Scaler variable type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 fn main() {
-
+    //formated output
     let a = 10;
     let b = 100;
     println!("Hello, world!, {} {}", a, b);
+
+    // unsigned integer
+    // u8, u16, u32, u64, u128
+    let unsigned: u8 = 100;
+
+    // signed interger 
+    // i8. i16, i32, i64, i128 
+    let signed: i8 = -100;
+
+    //float is used for decimals
+    let float: f32 = 1.0;
+
+    println!("unsign: {} sign: {} float: {}", unsigned, signed, float);
+
+    // char - can only be
+    let letter = "c";
+    let emoji = "\u{1F600}"; // :D
+    let is_true : bool = true;
+    println!("letter: {} emoji: {} isTrue: {}", letter, emoji, is_true);
+
 }
